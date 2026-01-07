0e90d9202192e4a5d8ac161c959b0c5871a51092
ShivrajGawande <shivrajgawande1279@gmail.com>
2026-01-07 11:13:38 +0530

    Initial commit

---
+++ src/main/java/com/upipaytrace/UpiPayTraceApplication.java
@@ -0,0 +1,13 @@
+package com.upipaytrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpiPayTraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpiPayTraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upipaytrace/UpiPayTraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upipaytrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpiPayTraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
